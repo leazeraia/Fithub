eb6e6a08c47c23822d9b6207f2c581497d79f0fe
leazeraia <116260131+leazeraia@users.noreply.github.com>
2023-03-31 09:29:50 +0200

    footer done w/ contact&legalNotice links (#10)
    * footer done w/ contact&legalNotice links
    
    * footer done w/ contact&legalNotice links

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+// import du fichier de style
+import './styles.scss';
+// import de la balise Link de react-router-dom
+import { Link } from 'react-router-dom';
+
+// création du composant Footer
+const Footer = () => (
+  // création de la balise footer, des liens et du paragraphe
+  <footer className="footer">
+    <Link to='/contacts' className="footer__link">Contacts</Link>
+    <Link to='/mentionslegales' className="footer__link">Mentions légales</Link>
+    <p className="footer__text">© 2023 - Tous droits réservés</p>
+  </footer>
+);
+
+// export du composant Footer
+export default Footer;
