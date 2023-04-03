4c9dcb1e1bfb4f7d9a535487bb1f1927cff88fb0
Yannick Huang <115977341+Ynck-Hng@users.noreply.github.com>
2023-04-03 12:58:41 +0200

    fix: fixed isAdmin middleware to work as intended (#15)
    * feat: activity controller (CREATE + READ) & routes
    
    * fix: removed category activity route because not implemented yet
    
    * fix: fixed isAdmin because it was always returning an error

--- backend/app/utils/userValidations/isAdmin.js
+++ backend/app/utils/userValidations/isAdmin.js
@@ -1,11 +1,15 @@
 const userValidations = require("debug")("utils:isAdmin");
 const isAdmin = (req, res, next) => {
     // Checks if there is a user and then if it's an admin
-    if(!req.session.user || req.session.user.role !== "admin"){
-        userValidations("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+    if(!req.session.user){
+        userValidations("Error, not authenticated.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
         return res.status(403).json("Access denied.");
     };
 
+    if(req.session.user.role !== "admin"){
+        userValidations("Error, not admin.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+        return res.status(403).json("Access denied.");
+    }
     next();
 };
 
