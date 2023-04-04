5f3b6baec6a9e095ef1503aceb6e09af5b438822
Yannick Huang <115977341+Ynck-Hng@users.noreply.github.com>
2023-04-04 15:57:39 +0200

    feat: user image management (#24)

--- backend/app/controllers/users/userController.js
+++ backend/app/controllers/users/userController.js
@@ -1,8 +1,10 @@
-const userError = require("debug")("controller:user");
+const userControllerError = require("debug")("controller:userControllerError");
 const { User } = require("./../../models");
 const emailValidator = require("email-validator");
 const bcrypt = require("bcrypt");
 const passwordChecker = require("../../utils/userValidations/passwordChecker");
+const fs = require("fs");
+const isSameIdAsUserSessionId = require("./../../utils/userValidations/isSameAsUserSessionId");
 
 const userController = {
     findAll: async (req, res) => {
@@ -12,10 +14,23 @@ const userController = {
                 exclude: ['password']
             },
         });
+
         // Must test result.length because findAll always returns [] if no users found
         if(result.length === 0){
-            return res.status(404).json("User cannot be found.");
+            userControllerError("Error, users cannot be found.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);;
+            return res.status(404).json("Users cannot be found.");
         };
+
+        result.map((user) => {
+            const userData = user.get({plain: true});
+            if(!userData.image_path){
+                return userData.dataURI = null;
+            };
+
+            const image = fs.readFileSync(`${userData.image_path}`);
+            const imageBuffer = Buffer.from(image).toString("base64");
+            userData.dataURI = `data:${userData.image_mimetype};base64,${imageBuffer}`;
+        })
 
         res.status(200).json(result);
     },
@@ -39,30 +54,40 @@ const userController = {
         });
         // if user not found, return 404
         if(!result){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, user cannot be found.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("User cannot be found.");
         };
+        
+        const resultDataWithImage = result.get({plain: true});
 
-        res.status(200).json(result);
+        if(resultDataWithImage.image_path){
+            const image = fs.readFileSync(`${result.image_path}`);
+            const imageBuffer = Buffer.from(image).toString("base64");
+            resultDataWithImage.dataURI = `data:${resultDataWithImage.image_mimetype};base64,${imageBuffer}`;
+        }
+
+        res.status(200).json(resultDataWithImage);
     },
 
     createOne: async (req, res) => {
-        const {firstname, lastname, nickname, phone, password, passwordConfirm, weight, email, gender} = req.body;
-
+        const {firstname, lastname, nickname, phone, password, passwordConfirm, weight, age, email, gender} = req.body;
         // checks that every fields have been properly received
-        if(!firstname || !lastname || !nickname || !password || !passwordConfirm || !weight || !email || !gender){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
-            return res.status(400).json("firstname, lastname, nickname, password, passwordConfirm, weight, email, gender are required.");
+        
+        if(!firstname || !lastname || !nickname || !password || !passwordConfirm || !weight || !email || !age){
+            userControllerError("Error, missing required fields.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            return res.status(400).json("firstname, lastname, nickname, password, passwordConfirm, weight, email, age are required.");
         }
+
         // checks if nickname is already taken
         const findUserNickname = await User.findOne({
             where: {
                 nickname
             }
         });
+
         // if taken, return 409 conflict
         if(findUserNickname){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, nickname already exists.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(409).json("Nickname already exists.");
         };
 
@@ -70,7 +95,7 @@ const userController = {
         const checkEmail = emailValidator.validate(email);
 
         if(!checkEmail){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, invalid email.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("Email is incorrect.");
         };
 
@@ -81,7 +106,7 @@ const userController = {
         });
 
         if(findUserEmail){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, email already used.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(409).json("Email already exists.");
         }
 
@@ -93,7 +118,7 @@ const userController = {
             });
 
             if(findUserPhone){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, phone number already linked to an account.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(409).json("Number already linked to an account.");
             }
 
@@ -102,7 +127,7 @@ const userController = {
 
         
         if(password !== passwordConfirm){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, password and passwordConfirm do not match.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(400).json("Password and passwordConfirm do not match.");
         };
 
@@ -111,12 +136,20 @@ const userController = {
         const checkPassword = passwordChecker(password);
 
         if(!checkPassword){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
-            return res.status(400).json("Secure your password with at least a capitalized letter, a symble and a number.");
+            userControllerError("Error, unsafe password.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            return res.status(400).json("Secure your password with at least a capitalized letter, a symbole and a number.");
         };
 
         // hash the password
         const hashedPassword = bcrypt.hashSync(password, 10);
+        let filePath;
+        let fileMimetype;
+        if(req.file !== undefined){
+            const sourceFile = req.file.path;
+            filePath = `uploads/${req.file.filename}`;
+            fs.copyFileSync(sourceFile, filePath);
+            fileMimetype = req.file.mimetype;
+        }
 
         const newUser = {
             firstname,
@@ -124,8 +157,11 @@ const userController = {
             nickname,
             password: hashedPassword,
             weight,
+            age,
             email,
-            gender
+            gender,
+            image_path: null || filePath,
+            image_mimetype: null || fileMimetype
         };
         // store in the database
         await User.create(newUser);
@@ -135,14 +171,18 @@ const userController = {
     updateOne: async (req, res) => {
         const userId = req.params.userId;
 
-        isSameIdOrAdmin(req, res, userId);
+        isSameIdAsUserSessionId(req, res, userId);
 
-        const {firstname, lastname, nickname, phone, password, passwordConfirm, role, weight, email, gender} = req.body;
+        const {firstname, lastname, nickname, phone, password, passwordConfirm, role, age, weight, email, gender} = req.body;
 
-        const findUser = await User.findByPk(userId);
+        const findUser = await User.findByPk(userId, {
+            attributes: {
+                exclude: ["password"]
+            }
+        });
 
         if(!findUser){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, user cannot be found.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("User cannot be found.");
         };
 
@@ -157,13 +197,14 @@ const userController = {
 
         if(phone){
             const findUserPhone = await User.findOne({
+                exclude: ["password"],
                 where: {
                     phone
                 }
             });
 
             if(findUserPhone){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, phone number already linked to an account.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(409).json("Number already linked to an account.");
             }
 
@@ -172,13 +213,14 @@ const userController = {
 
         if(nickname){
             const findUserNickname = await User.findOne({
+                exclude: ["password"],
                 where: {
                     nickname
                 }
             });
 
             if(findUserNickname){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, nickname already taken.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(409).json("Nickname already exists.");
             };
 
@@ -190,13 +232,13 @@ const userController = {
         if(password){
 
             if(password !== passwordConfirm){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, password and passwordConfirm do not match.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(400).json("Password and passwordConfirm do not match.");
             };
 
             const checkPassword = passwordChecker(password);
             if(!checkPassword){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, unsafe password.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(400).json("Secure your password with at least a capitalized letter, a symbol and a number.");
             };
 
@@ -218,18 +260,19 @@ const userController = {
             const checkEmail = emailValidator.validate(email);
             
             if(!checkEmail){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, invalid email.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(409).json("Email is incorrect.");
             };
             
             const findUserEmail = await User.findOne({
+                exclude: ["password"],
                 where: {
                     email
                 }
             });
             
             if(findUserEmail){
-                userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+                userControllerError("Error, email already used.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
                 return res.status(409).json("Email already exists.");
             };
             
@@ -240,6 +283,18 @@ const userController = {
             findUser.gender = gender;
         };
 
+        if(age){
+            findUser.age = age;
+        };
+
+        if(req.file){
+            const sourceFile = req.file.path;
+            let filePath = `uploads/${req.file.filename}`;
+            fs.copyFileSync(sourceFile, filePath);
+            findUser.image_path = filePath;
+            findUser.image_mimetype = req.file.mimetype;
+        }
+
         // update the user data
         await findUser.save();
 
@@ -249,10 +304,14 @@ const userController = {
     deleteOne: async (req, res) => {
         const userId = req.params.userId;
 
-        const findUser = await User.findByPk(userId);
+        const findUser = await User.findByPk(userId, {
+            attributes: {
+                exclude: ["password"]
+            }
+        });
         // make sure that user exists before deleting
         if(!findUser){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, user cannot be found.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("User cannot be found.");
         };
 
@@ -265,19 +324,18 @@ const userController = {
         // If user already logged in, then return 403 forbidden
         const findSession = req.session.user;
         if(findSession){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, already logged in.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(403).json("Access denied, user is already logged in.");
-        }
+        };
 
         const {email, password} = req.body;
         // make sure that both email & password have been sent
         if(!email || !password){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, email or password missing.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(400).json("Email and password are both required.");
         };
         // make sure that user exists
         const findUser = await User.findOne({
-            exclude: ["password"],
             where:{
                 email
             }
@@ -286,21 +344,21 @@ const userController = {
         // if user does not exist, return 404
 
         if(!findUser){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, email or password is incorrect.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("Email or password is incorrect.");
         };
 
         const isPasswordCorrect = bcrypt.compare(password, findUser.password);
         
         if(!isPasswordCorrect){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, email or password is incorrect.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(400).json("Email or password is incorrect.");
         };
         // creates a user session
         req.session.user = {
             id: findUser.id,
             role: findUser.role
-        }
+        };
 
         // to discuss what info we want to transmit to the client
         res.status(200).json(findUser);
@@ -311,9 +369,13 @@ const userController = {
         const findSession = req.session.user;
         
         // make sure that user exists before logging out
-        const findUser = await User.findByPk(findSession.id);
+        const findUser = await User.findByPk(findSession.id, {
+            attributes: {
+                exclude: ["password"]
+            }
+        });
         if(!findUser){
-            userError("Error", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
+            userControllerError("Error, user cannot be found.", `path : ${req.protocol}://${req.get("host")}${req.originalUrl}`);
             return res.status(404).json("User cannot be found.");
         };
 
@@ -321,7 +383,7 @@ const userController = {
 
         res.status(200).json("Logged out !");
         
-    }
+    },
 }
 
 module.exports = userController;
