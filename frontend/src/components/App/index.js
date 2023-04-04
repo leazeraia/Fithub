7e25f5b61fc3c89c5720adcc6335bc4fc518880f
taphacamara <116665295+taphacamara@users.noreply.github.com>
2023-04-04 10:58:53 +0200

    page home avec header et formulaire de connexion encours de construction (#11)
    * page home avec header et formulaire de connexion encours de construction
    
    * fin de la home, header et form connexion
    
    * ajoue des routes link and formulair resetPassword
    
    * ajout hearder dans app
    
    ---------
    
    Co-authored-by: Yannick Huang <115977341+Ynck-Hng@users.noreply.github.com>

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,13 +1,34 @@
 // == Import
-import './styles.scss';
+
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import './styles.css';
+//import du dossier home
+import Home from '../Home'; 
+import ResetPassword from "../resetPassword";
+import Header from "../Header";
 import Footer from '../Footer';
 
 // == Composant
 function App() {
-  return (
-   < Footer />
+  return (  
+    <BrowserRouter>
+      <Header/>  
+      <Routes>
+
+         <Route path="/" element={<Home/>} />
+         <Route path="/signup" element={"<h1>Hello</h1>"}/>
+         <Route path="/reset-password" element={<ResetPassword/>} />
+
+      </Routes> 
+      <Footer />
+    </BrowserRouter>
   );
 }
-
+     
 // == Export
 export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import logo from '../../assets/images/Logo_black.png'
+import buttonLogin from '../../assets/images/img-signin.png'
+import './header.css';
+import { json, Link } from 'react-router-dom';
+
+function Header() {
+    
+    //const Search = () => {
+        const [isFormVisible, setisFormVisible] = useState(false);
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('')
+        const toggleFormVisibility = () => { 
+            setisFormVisible(!isFormVisible);
+        }
+
+        
+
+        const handleSubmit = async (event) => {
+            event.preventDefault();
+
+            const formData = new FormData();
+            formData.append("email", email);
+            formData.append("password", password);
+     
+        const result = fetch("",{
+            headers:{"Content-Type": "application/json"},
+            method: "post",
+            body: JSON.stringify(formData)
+        })
+       
+    
+    }
+        
+
+        
+
+  return (
+     <nav className='header-nav'>
+        <div className='logo-nav'>
+            <Link to="/" className='link-logo-nav'>
+             <img src={logo} alt='logo FitHub'/>
+            </Link> 
+               
+        </div>
+
+        <div className='link-nav'>
+           <ul>
+            <li> <Link to="/signup"> S'inscrire</Link></li>
+            
+            <li className='login-nav'>
+                    <p> Se connecter </p> 
+                <img src={buttonLogin} onClick={toggleFormVisibility} className='button-image-nav' alt='button se connecter'/>
+                {isFormVisible && (
+                <form onSubmit={handleSubmit} className='form-login'>
+                    <div className='form-group'>
+                        <div>
+                            <label htmlFor='email' >E-mail</label>
+                        </div>
+                        <input type='email' value={email} onChange={(event)=> setEmail(event.target.value)} name='email' id='email'/>
+                    </div>
+
+                    <div className='form-group form-group-password' >
+                        <div>
+                            <label htmlFor='passeword' >Mot de Passe</label>
+                        </div>
+                        <input type='passeword' value={password} onChange={(event)=> setPassword(event.target.value)} name='passeword' id='passeword'/>
+                    </div>
+                    <Link to="/reset-password" className='passwordforget'>
+                        Mot de passe oublié
+                    </Link>
+                    <div className='btn-submit'>
+                        <button type='submit'> Connecter</button>
+                    </div>
+
+                </form>
+                )}
+                
+            </li>
+           </ul> 
+        </div>
+
+     </nav>
+  )
+}
+
+export default Header ;
