ed4448d8d518771ab363b3a616dac19bf31e965b
leazeraia <lea.zerdem75@gmail.com>
2023-04-19 11:31:33 +0200

    branch cleaned w/ back

--- backend/app/utils/userValidations/passwordChecker.js
+++ backend/app/utils/userValidations/passwordChecker.js
@@ -1,7 +1,7 @@
 const passwordChecker = (inputPassword) => {
     // checks that the password contains at least :
     // 1 symbol, 1 capitalized letter, 1 number and has more than 8 letters
-    const regex = /^(?=.*[A-Z])(?=.*[&@!$#*])(?=.*[0-9]).{8,50}$/;
+    const regex = /^(?=.*[A-Z])(?=.*[&@!$#*.])(?=.*[0-9]).{8,50}$/;
     return regex.test(inputPassword);
 };
 
